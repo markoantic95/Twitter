b3847d1d1607d688e87181474b0351d641c07d48
markoantic95 <markoantic95@gmail.com>
2016-04-03 23:30:16 +0200

    Napisana je JavaDoc dokumentacija za klasu TwitterPoruka, njene atribute i metode.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -2,27 +2,54 @@ package com.twitter.poruke;
 
 public class TwitterPoruka {
 	
+	/**
+	 * Ime korisnika
+	 */
 	private String korisnik;
+	/**
+	 * Tekst poruke
+	 */
 	private String poruka;
+	/**
+	 * Vraca ime korisnika 
+	 * @return korisnik
+	 */
 	public String getKorisnik() {
-	return korisnik;
+		return korisnik;
 	}
+	/**
+	 * Metoda postavlja vrednost za ime korisnika
+	 * @param korisnik - ime korisnika
+	 * @throws java.lang.RuntimeException ako je kao parametar prosledjen null ili prazan String
+	 */
 	public void setKorisnik(String korisnik) {
-	if (korisnik==null || korisnik == "")
-	throw new RuntimeException(
-	"Ime korisnika mora biti uneto");
-	this.korisnik = korisnik;
+		if (korisnik==null || korisnik == "")
+		throw new RuntimeException(
+			"Ime korisnika mora biti uneto");
+		this.korisnik = korisnik;
 	}
+	/**
+	 * Vraca tekst poruke
+	 * @return poruka
+	 */
 	public String getPoruka() {
-	return "poruka";
+		return "poruka";
 	}
+	/**
+	 * Metoda postavlja sadrzaj poruke
+	 * @param poruka
+	 * @throws java.lang.RuntimeException ako je kao parametar prosledjen null ili je duzina String-a veca od 140
+	 */
 	public void setPoruka(String poruka) {
-	if (poruka==null || this.poruka.length()>140)
-	throw new RuntimeException(
-	"Poruka mora biti uneta i mora imati najvise 140 znakova");
-	this.poruka = poruka;
+		if (poruka==null || this.poruka.length()>140)
+			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
+		this.poruka = poruka;
 	}
+	
+	/**
+	 * @return ime korisnika i tekst poruke
+	 */
 	public String toString(){
-	return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
+		return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
 	} 
 }
